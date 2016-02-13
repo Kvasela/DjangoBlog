a9e5c63aa7570af77af81aa643fb23eb7fb0aebe
Kvasela <kvasela@gmail.com>
2016-02-13 12:54:05 +0200

    add list_display fields and search_fields and change fields into fieldsets

--- blog/admin.py
+++ blog/admin.py
@@ -5,20 +5,15 @@ from .models import Article, Tag
 class ArticleAdmin(admin.ModelAdmin):
     fieldsets = (
         (None, {
-            'fields': ('title',)
-         }),
-        ('Короткий зміст статті', {
-            'classes': ('collapse',),
-            'fields': ('excerpt',)
-         }),
-        (None, {
-            'fields': ('article', 'slug', 'tags', 'publish',)
+            "fields": ("title", "excerpt", "article", "slug", "tags", "publish",)
          }),
     )
     prepopulated_fields = {
         "slug": ("title",)
     }
     list_filter = ("publish", "created", "modified")
+    list_display = ("title", "created", "modified")
+    search_fields = ["title"]
 
 admin.site.register(Article, ArticleAdmin)
 admin.site.register(Tag)
