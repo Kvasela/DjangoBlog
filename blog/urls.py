4f7be2e41e1e86a7d62895045b0ef62dd74ba50d
Kvasela <kvasela@gmail.com>
2016-02-13 12:47:40 +0200

    add serch form into main menu, search template, serach url add logo django and styles for search form

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
-from .views import IndexView, ArticleView, TagView
+from .views import IndexView, ArticleView, TagView, SearchView
 
 
 urlpatterns = (
     url(r'^$', IndexView.as_view(), name="home"),
+    url(r'^search/?$', SearchView.as_view(), name="search"),
     url(r'^article/(?P<slug>\S+)$', ArticleView.as_view(), name="article"),
     url(r'^tag/(?P<tag>\S+)$', TagView.as_view(), name="tag"),
 )
--- blog/views.py
+++ blog/views.py
@@ -11,7 +11,7 @@ logger = logging.getLogger(__name__)
 class IndexView(ListView):
     queryset = Article.objects.published()
     template_name = "index.html"
-    # paginate_by = 2
+    paginate_by = 5 
 
 
 class ArticleView(DetailView):
@@ -21,13 +21,28 @@ class ArticleView(DetailView):
 
 class TagView(ListView):
     template_name = "articles_by_tag.html"
+    paginate_by = 5
 
     def get_queryset(self):
         slug = self.kwargs['tag']
         tag = get_object_or_404(Tag, slug=slug)
-        return Article.objects.filter(tags=tag.id)
+        return Article.objects.filter(tags=tag.id, publish=True)
 
     def get_context_data(self, **kwargs):
         context = super(TagView, self).get_context_data(**kwargs)
         context['tag'] = self.kwargs['tag']
+        return context
+
+
+class SearchView(ListView):
+    template_name = "search.html"
+    paginate_by = 5
+
+    def get_queryset(self):
+        q = self.request.GET.get('q', None)
+        return Article.objects.filter(title__icontains=q)
+
+    def get_context_data(self, **kwargs):
+        context = super(SearchView, self).get_context_data(**kwargs)
+        context['q'] = q = self.request.GET.get('q', None)
         return context
