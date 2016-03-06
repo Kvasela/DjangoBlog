3b9a437b3f5cb49972ba1a4a9a84d01eaec8aedf
Kvasela <kvasela@gmail.com>
2016-03-07 01:39:05 +0200

    change binding static in urlpatterns

--- DjangoBlog/urls.py
+++ DjangoBlog/urls.py
@@ -25,4 +25,7 @@ urlpatterns = [
     url(r'^about/', AboutView.as_view(), name="about"),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^ckeditor/', include('ckeditor_uploader.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
